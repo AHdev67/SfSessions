cfba95536046c541157c04412e8f16b4556acbf1
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-24 15:36:19 +0200

    improved visibility of intern form type

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,33 +4,72 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('dateNaissance', null, [
-                'widget' => 'single_text',
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
             ])
-            ->add('tel')
-            ->add('email')
-            ->add('adresse')
-            ->add('codePostal')
-            ->add('ville')
-            ->add('dateInscription', null, [
-                'widget' => 'single_text',
+
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
             ])
-            ->add('sessions', EntityType::class, [
-                'class' => Session::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+            
+            ->add('dateNaissance', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('tel', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('codePostal', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ]
+            ])
+
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
             ])
         ;
     }
