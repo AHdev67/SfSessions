5ac6f53e5f6fd59083add5b02f81b8dc89467473
AHdev67 <antoinehelfer.contact@gmail.com>
2024-04-26 16:38:19 +0200

    added methods to display user list and manipulate user roles

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,5 +31,37 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    //---------------------------------------------------
+    //METHODE USERS QUI RENVOIE LA LISTE DES UTILISATEURS
+    //---------------------------------------------------
+    #[Route('/security', name: 'app_users')]
+    public function index(UtilisateurRepository $utilisateurRepository): Response
+    {
+        $utilisateurs = $utilisateurRepository->findBy([], ["email" => "DESC"]);
+        return $this->render('security/utilisateurs.html.twig', [
+            'controller_name' => 'SessionController',
+            'utilisateurs' => $utilisateurs
+        ]);
+    }
+
+    #[Route('/security/utilisateurs/{id}/grant', name: 'grant_admin')]
+    public function grant(Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
+    {
+        if (in_array("ROLE_ADMIN", $utilisateur->getRoles()))
+        {
+            $utilisateur->removeRole("ROLE_ADMIN");
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+        }
+        else
+        {
+            $utilisateur->addRole("ROLE_ADMIN");
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_users');
     }
 }
